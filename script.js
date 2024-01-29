71a0f408e1758a6ccb85311f7086ade8a736d237
Joel <joel.brookes@gmail.com>
2024-01-29 20:02:31 +1100

    Added for loop to create div grid

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let test = console.log("Hello World");
-test;
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        let div = document.createElement("div");
+        div.style.width = "25px";
+        div.style.height = "25px";
+        div.style.background = "red";
+        div.style.border = "solid"
+        document.querySelector(".grid-container").appendChild(div);
+    }
+    let jump = document.createElement("br");
+    document.querySelector(".grid-container").appendChild(jump);
+}
