0aed0d4e5a9803dbe451b16e4515b9ed083d7c4e
Joel <joel.brookes@gmail.com>
2024-02-01 20:43:06 +1100

    JS function now creates <div> grid

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-        let div = document.createElement("div");
-        div.style.width = "25px";
-        div.style.height = "25px";
-        div.style.background = "red";
-        div.style.border = "solid"
-        document.querySelector(".grid-container").appendChild(div);
-    }
-    let jump = document.createElement("br");
-    document.querySelector(".grid-container").appendChild(jump);
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         let div = document.createElement("div");
+//         div.style.width = "25px";
+//         div.style.height = "25px";
+//         div.style.background = "red";
+//         div.style.border = "solid"
+//         document.querySelector(".grid-container").appendChild(div);
+//     }
+//     let jump = document.createElement("br");
+//     document.querySelector(".grid-container").appendChild(jump);
+// }
+
+
+
+function genDivs(v) { 
+    let e = document.querySelector(".grid-container"); // whatever you want to append the rows to: 
+    for (let i = 0; i < v; i++) { 
+         let row = document.createElement("div"); 
+         row.className = "row";
+        //  row.style.width = "25px" 
+        //  row.style.height = "100px" 
+         row.style.backgroundColor = "white"
+         for(let x = 1; x <= v; x++) { 
+            let cell = document.createElement("div"); 
+            cell.className = "gridsquare";
+            cell.style.backgroundColor = "beige";
+            cell.style.border = "none"; 
+            cell.style.height = "0.3em";
+            cell.style.width = "0.3em";
+            row.appendChild(cell); 
+        } 
+        e.appendChild(row); 
+    } 
+    // document.querySelector("code").innerText = e.innerHTML;
 }
+
+genDivs(100);
+
+let sketch = document.querySelector(".gridsquare");
+sketch.addEventListener("mouseover", () => {
+    sketch.style.backgroundColor = "black";}
+);
