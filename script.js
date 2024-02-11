4bd6488ca2386d4211f120c4fcc301aba769b2d1
Joel <joel.brookes@gmail.com>
2024-02-11 23:35:06 +1100

    Added actions to buttons

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
  function genDivs(v) { 
     let e = document.querySelector(".grid-container"); // whatever you want to append the rows to: 
+    let normalBtn = document.querySelector(".normal");
+    let rainbowBtn = document.querySelector(".rainbow");
     let resetBtn = document.querySelector(".reset");
     for (let i = 0; i < v; i++) { 
          let row = document.createElement("div"); 
          row.className = "row";
-        //  row.style.width = "25px" 
-        //  row.style.height = "100px" 
+        //  row.style.width = "0.3em" 
+        //  row.style.height = "0.3em" 
          row.style.backgroundColor = "white"
          for(let x = 1; x <= v; x++) { 
             let cell = document.createElement("div"); 
@@ -16,19 +18,28 @@
             cell.style.width = "0.3em";
             row.appendChild(cell); 
             cell.addEventListener("mouseover", () => {
-                cell.style.backgroundColor = "black";}
-            );
+                cell.style.backgroundColor = "black";
+            });
+            normalBtn.addEventListener("click", () => {
+                cell.addEventListener("mouseover", () => {
+                    cell.style.backgroundColor = "black";
+            });
+            });
+            rainbowBtn.addEventListener("click", () => {
+                cell.addEventListener("mouseover", () => {
+                    cell.style.backgroundColor = "green";
+            });
             resetBtn.addEventListener("click", () => {
                 cell.style.backgroundColor = "beige";
+            });
+           
             });
         } 
         e.appendChild(row); 
     } 
-    // document.querySelector("code").innerText = e.innerHTML;
 }
 
-// let resetBtn = document.querySelector(".reset");
 
 
-genDivs(50);
+genDivs(100);
 
